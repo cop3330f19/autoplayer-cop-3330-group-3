537a0f847d328ccee946b1d4a7ff91982e8e6cbb
TheGreatPerhaps <61095672+TheGreatPerhaps@users.noreply.github.com>
2020-04-20 20:06:40 -0400

    Update Playlist.h

--- Playlist.h
+++ Playlist.h
@@ -4,39 +4,39 @@ Group 3 Members: Hunter Bell, Tariq Green, Darrell Brown, Christian Pierre Paul
 Date Last Edited: 04/20/20
 */
 
-		#ifndef PLAYLIST_H
-		#define PLAYLIST_H
-    #include <cstdlib>
-    #include <string>
-		#include "Song.h"
-		#include <iostream>
-		#include <vector>
+#ifndef PLAYLIST_H
+#define PLAYLIST_
+#include <cstdlib>
+#include <string>
+#include "Song.h"
+#include <iostream>
+#include <vector>
 	
-		class Playlist
-		{
-		private:
-		static char mode;
-		int currentsong;
-		std::string Pname;
-    std::vector<Song> songs;
+class Playlist
+{
+private:
+static char mode;
+int currentsong;
+std::string Pname;
+std::vector<Song> songs;
 		
-		public:
-		void addSong(Song &);
-		void deleteSong(Song &);
-		Playlist intersect(Playlist &);
-    void play();
-		static void mode(char);
-		bool linearsearch(std::vector<Song>, Song &);
-    friend Playlist operator+(Playlist &, Song &);
-		friend Playlist operator+(Playlist &, Playlist &);
-		friend Playlist operator-(Playlist &, Song &);
-		friend std::ostream &operator<<(std::ostream &, const Playlist &);
-		std::vector<Song> getplaylistsongs();
-		void setPlaylistName(std::string);
-		std::string getPlaylistName();
-    Playlist();
-		Playlist(std::string);
-		};
-		#endif
+public:
+void addSong(Song &);
+void deleteSong(Song &);
+Playlist intersect(Playlist &);
+void play();
+static void mode(char);
+bool linearsearch(std::vector<Song>, Song &);
+friend Playlist operator+(Playlist &, Song &);
+friend Playlist operator+(Playlist &, Playlist &);
+friend Playlist operator-(Playlist &, Song &);
+friend std::ostream &operator<<(std::ostream &, const Playlist &);
+std::vector<Song> getplaylistsongs();
+void setPlaylistName(std::string);
+std::string getPlaylistName();
+Playlist();
+Playlist(std::string);
+};
+#endif
 
 
