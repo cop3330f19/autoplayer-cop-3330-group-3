fb8d444315c82b01cb313a3fab48fae0e7d70fe9
TheGreatPerhaps <61095672+TheGreatPerhaps@users.noreply.github.com>
2020-04-22 13:15:24 -0400

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,3 +1,10 @@
+
+/*
+Playlist.cpp
+Group 3 Members: Hunter Bell, Tariq Green, Darrell Brown, Christian Pierre Paul
+Date Last Edited: 04/20/20
+*/
+
 #include <iostream>
 #include "Playlist.h"
 #include "Song.h"
